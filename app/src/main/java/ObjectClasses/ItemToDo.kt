100d960ab32d0f8586f7eba143394b4c5e931769
Padpatine <padamay05@gmail.com>
2026-06-25 19:16:27 +0200

    ItemToDo et ListeToDo : passage en entities
    Et userDao...

--- app/src/main/java/ObjectClasses/ItemToDo.kt
+++ app/src/main/java/ObjectClasses/ItemToDo.kt
@@ -1,9 +1,16 @@
 package ObjectClasses
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "items")
 class ItemToDo(
-    val id : String? = null,
+    @PrimaryKey val id : String? = null,
     val description : String? = null,
-    var fait : Boolean = false
+    var fait : Boolean = false,
+    val listId: String,         // Clé étrangère vers la liste parente
+    var toSync: Boolean = false  // True si modifié hors ligne
 ) {
 
     override fun toString(): String {
--- app/src/main/java/ObjectClasses/ListeToDo.kt
+++ app/src/main/java/ObjectClasses/ListeToDo.kt
@@ -1,21 +1,28 @@
 package ObjectClasses
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Ignore
+
+@Entity(tableName = "lists")
 class ListeToDo(
-    public var id: String? = null,
-    public var titreListeToDo: String,
-    public var lesItems: MutableList<ItemToDo>,
+    @PrimaryKey var id: String? = null,
+    var titreListeToDo: String,
 ) {
-
-    private fun RechercherItem(description : String) : ItemToDo{
-        for (i in 0..(lesItems.size)) {
-            if (lesItems[i].description == description) {
-                return lesItems[i]
-            }
-        }
-        return  ItemToDo(null, null, false)
-    }
-
-    override fun toString(): String {
-        return ("la liste ${titreListeToDo} contient : ${lesItems}")
-    }
+    @Ignore
+    var lesItems: MutableList<ItemToDo> = mutableListOf()
 }
+
+
+//    private fun RechercherItem(description : String) : ItemToDo{
+//        for (i in 0..(lesItems.size)) {
+//            if (lesItems[i].description == description) {
+//                return lesItems[i]
+//            }
+//        }
+//        return  ItemToDo(null, null, false)
+//    }
+//
+//    override fun toString(): String {
+//        return ("la liste ${titreListeToDo} contient : ${lesItems}")
+//    }
