4c689eeb95b2f41c797c5059f205c81f44f56d53
Padpatine <padamay05@gmail.com>
2026-06-25 20:12:16 +0200

    Commentaire

--- app/src/main/java/com/example/tea1/userDao.kt
+++ app/src/main/java/com/example/tea1/userDao.kt
@@ -3,21 +3,21 @@ package com.example.tea1
 import androidx.room.*
 import ObjectClasses.ListeToDo
 
-@Dao
-public interface UserDao {1
-    1@Query("SELECT * FROM user")
-    List<User> getAll();
-
-    2@Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-
-    3@Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
-
-    4@Insert
-    void insertAll(User... users);
-
-    5@Delete
-    void delete(User user);
-}1
+//@Dao
+//public interface UserDao {1
+//    1@Query("SELECT * FROM user")
+//    List<User> getAll();
+//
+//    2@Query("SELECT * FROM user WHERE uid IN (:userIds)")
+//    List<User> loadAllByIds(int[] userIds);
+//
+//    3@Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
+//            "last_name LIKE :last LIMIT 1")
+//    User findByName(String first, String last);
+//
+//    4@Insert
+//    void insertAll(User... users);
+//
+//    5@Delete
+//    void delete(User user);
+//}1
